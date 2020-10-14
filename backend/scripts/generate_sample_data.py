58bf9dae985075af361b56898a321cfa58b063f8
Nick-Bosworth <nicholasbosworth014@gmail.com>
2020-10-14 18:09:30 +1100

    generated 1000 samples

--- backend/scripts/generate_sample_data.py
+++ backend/scripts/generate_sample_data.py
@@ -1,52 +1,76 @@
 import pandas as pd
 import random
+import json
+import os
+import math
 
 types = ['casual', 'full time', 'part time', 'internship', 'apprenticeship']
-
-field = ['business', 'science', 'engineering', 'law', 'arts', 'journalism']
 
 education = ['none', 'secondary', 'tertiary',
              'masters', 'PhD', 'apprenticeship', 'MBA']
 
-skills = ['language proficiency', 'maths', 'law', 'programming', 'writing', 'management', 'research', 'databases',
-          'communication', 'presentation', 'marketing', 'creativity', 'persuasion', 'animation', 'journalism', 'leadership']
-
+fields = []
+skills = []
+with open('backend\scripts\category.json') as f:
+  categories = json.load(f)
+  for field in categories:
+      fields.append(field['name'])
+      skills.extend(field['children'])
 
 def generate_candidate(i):
+    # Candidates have: first name, last name, email, password, group (cluster), education and skills
     candidate = []
-    candidate.append('candidate '+str(i))
-    candidate.append(random.sample(types, k=1))
-    candidate.append(random.sample(field, k=1))
+    candidate.append('c'+str(i)+' first name')
+    candidate.append('c'+str(i)+' last name')
+    candidate.append('c'+str(i)+'@email.com')
+    candidate.append('password')
+    candidate.append(0)
     candidate.append(random.sample(education, k=1))
     candidate.append(random.sample(skills, k=random.randint(1, 5)))
     return candidate
 
 
 def generate_job(i):
+    # Jobs have companies, names, salary ranges, types, skills and required education level
     job = []
-    job.append('company ' + str(i % 10))
+    job.append('company ' + str(math.floor(i/10)))
     job.append('job '+str(i))
+    salary = [random.random()*10000, random.random()*10000]
+    job.append(max(salary))
+    job.append(min(salary))
     job.append(random.sample(types, k=1))
-    job.append(random.sample(field, k=1))
     job.append(random.sample(education, k=1))
     job.append(random.sample(skills, k=random.randint(1, 5)))
     return job
 
+def generate_company(i):
+    # Companies have names, locations, industries and description
+    company = []
+    company.append('company ' + str(math.floor(i/10)))
+    company.append('company ' + str(math.floor(i/10)) + ' location')
+    company.append(random.sample(fields, k=1))
+    return company
 
 candidates = []
 jobs = []
+companies = []
 
 n = int(input("number to generate: "))
 
 for i in range(0, n):
     candidates.append(generate_candidate(i))
     jobs.append(generate_job(i))
+    if (i % 10 == 0):
+        companies.append(generate_company(i))
 
 job_df = pd.DataFrame(
-    jobs, columns=['company', 'name', 'type', 'field', 'education', 'skills'])
+    jobs, columns=['company', 'name', 'salary_top', 'salary_bottom', 'field', 'education', 'skills'])
 
 candidate_df = pd.DataFrame(
-    candidates, columns=['name', 'type', 'field', 'education', 'skills'])
+    candidates, columns=['first_name', 'last_name', 'email', 'password', 'group','education', 'skills'])
+
+company_df = pd.DataFrame(companies, columns=['name', 'location', 'industry'])
 
 job_df.to_csv('jobs.csv', index=False)
 candidate_df.to_csv('candidates.csv', index=False)
+company_df.to_csv('company.csv', index=False)
