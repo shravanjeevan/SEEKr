002bdb589155a5c6f7dded8daa7e87c0fda8ff42
cloudyaaron <cloudyaaron@users.noreply.github.com>
2020-10-20 11:15:32 +1100

    test

---
+++ reactfront/src/Register.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+function test(){
+    return(<>
+        <button>"yo"</button>
+        <input></input>
+    </>);
+}
+export default function register(){
+    return(
+        <div>
+            <test />
+
+        </div>
+    )
+}
--- reactfront/src/index.js
+++ reactfront/src/index.js
@@ -2,11 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import register from "./Register";
 
 const rootElement = document.getElementById("root");
+const testElement = document.getElementById("test");
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
+
   </React.StrictMode>,
   rootElement
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <register />
+
+  </React.StrictMode>,
+  testElement
 );
