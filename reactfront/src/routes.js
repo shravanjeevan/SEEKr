f38db1d4da41ff23665c512658df1e7706824c22
Maliha Mian <m.mian@unsw.edu.au>
2020-10-30 22:03:31 +1100

    added sign in to routes

--- reactfront/src/routes.js
+++ reactfront/src/routes.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import SignUpPage from "./pages/signup";
+import SignInPage from "./pages/signin";
 import ContactPage from "./pages/contact";
 import AboutPage from "./pages/about";
 import FaqsPage from "./pages/faqs";
@@ -16,6 +17,7 @@ class App extends Component {
       <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path="/signup" component={SignUpPage} />
+          <Route exact path="/signin" component={SignInPage} />
           <Route exact path="/contact" component={ContactPage} />
           <Route exact path="/about" component={AboutPage} />
           <Route exact path="/faqs" component={FaqsPage} />
