ffd8ac2ec0889e078b300ee5ff50c0ff98d934e6
niki <niki.bolourchi@gmail.com>
2020-11-03 00:22:34 +1100

    change of tables

--- reactfront/src/pages/contact.js
+++ reactfront/src/pages/contact.js
@@ -18,75 +18,105 @@ import { Delete } from "@material-ui/icons";
 import 'bootstrap/dist/css/bootstrap.css';
 import tableIcons from "./TableIcons.js";
 import MaterialTable from "material-table";
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+import { forwardRef } from 'react';
 
 
 const ContactPage= () => {
-  const results = [...Array(50).keys()].map(i => {
-  return {
-    name: faker.name.jobTitle(),
-    state: faker.random.number({
-    'min': 82,
-    'max': 99
-})
+  const { useState } = React;
+
+  const [columns, setColumns] = useState([
+    { title: 'Name', field: 'name' },
+    { title: 'Surname', field: 'surname', initialEditValue: 'initial edit value' },
+    { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+    {
+      title: 'Birth Place',
+      field: 'birthCity',
+      lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+    },
+  ]);
+  const [data, setData] = useState([
+    { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+    { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+  ]);
+
+  const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
   };
-});
-  const [data, setData] = useState(results);
-  const handleDeleteRows = (event, rowData) => {
-    let _data = [...data];
-    rowData.forEach(rd => {
-      _data = _data.filter(t => t.tableData.id !== rd.tableData.id);
-    });
-    setData(_data);
-  };
-  const handleApply= (event, rowData) => {
-    let _data = [...data];
-    rowData.forEach(rd => {
-      _data = _data.filter(t => t.tableData.id !== rd.tableData.id);
-    });
-    setData(_data);
-  };
-  
+ 
 
   return (
   
-  <div>
-  <h1> SEEKER </h1>
-   
-    <MaterialTable
-      title="Select row(s) to get the option to delete"
-      columns={[
-        {
-          title: "Name",
-          field: "name"
-        },
-        {
-          title: "Percentage",
-          field: "state"
-        }
-      ]}
+
+<MaterialTable
+      title="Editable Preview"
+      columns={columns}
       data={data}
-      options={{
-        selection: true,
-        pageSize: 10
+      editable={{
+        onRowAdd: newData =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              setData([...data, newData]);
+              
+              resolve();
+            }, 1000)
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              const dataUpdate = [...data];
+              const index = oldData.tableData.id;
+              dataUpdate[index] = newData;
+              setData([...dataUpdate]);
+
+              resolve();
+            }, 1000)
+          }),
+        onRowDelete: oldData =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              const dataDelete = [...data];
+              const index = oldData.tableData.id;
+              dataDelete.splice(index, 1);
+              setData([...dataDelete]);
+              
+              resolve()
+            }, 1000)
+          }),
       }}
       icons={tableIcons}
-      actions={[
-        {
-          icon: () => <Delete />,
-          tooltip: "Delete Rows",
-          onClick: handleDeleteRows
-        },
-        {
-          icon: () => <Delete />,
-          tooltip: "Delete Rows",
-          onClick: handleDeleteRows
-        }
-      ]}
     />
-    
-    
-    
-   </div>
+
   
   );
 }
