f15aae7c4452148851c3a62832eddfd46d67c446
cloudyaaron <cloudyaaron@users.noreply.github.com>
2020-11-10 10:12:25 +1100

    recieve data from backend for matched jobs

--- reactfront/src/pages/MatchedJob.jsx
+++ reactfront/src/pages/MatchedJob.jsx
@@ -11,12 +11,21 @@ import TextField from '@material-ui/core/TextField';
 
 function Matchlist(){
     const h = useHistory()
-
+    const [uid,setuid]=useState()
     useEffect(() => {
         if (cookies.load("t") === "" || cookies.load("t") === undefined) {
             h.push("/signin")
         }
-        fetch('http://localhost:8000/job_match/list/', {
+        fetch('http://127.0.0.1:8000/user/info/', {
+            method: "get",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': "Token " + cookies.load("t")
+            }
+        }).then(res => res.json()).then((data => {
+            console.log(data)
+            setuid(data.account.id)
+            fetch('http://localhost:8000/job_match/list/'+data.account.id, {
             method: "get",
             headers: {
                 'Content-Type': 'application/json',
@@ -32,6 +41,15 @@ function Matchlist(){
                 console.log(error.status + error.statusText)
             }
         })
+        })).
+        catch(error => {
+            if (error.status === 404) {
+                console.log(error.status + error.statusText)
+            } else if (error.status === 403) {
+                console.log(error.status + error.statusText)
+            }
+        })  
+        
     },[])
 
 
